814749ced107243f016b79eeba0187136d58b25d
Jinki Hou <jhou@smith.edu>
2024-10-29 09:50:44 -0400

    set up inheritence

--- House.java
+++ House.java
@@ -1,12 +1,20 @@
 /* This is a stub for the House class */
-public class House {
 
-  public House() {
+import java.util.ArrayList;
+
+public class House extends Building {
+
+  private ArrayList<String> residents;
+  private boolean hasDiningRoom;
+
+  public House(String name, String address, int nFloors) {
+    super(name, address, nFloors);
     System.out.println("You have built a house: 🏠");
+    
   }
 
   public static void main(String[] args) {
-    new House();
+    new House("morris", "Northampton", 3);
   }
 
 }
