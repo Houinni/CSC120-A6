36c76dd1d11287587058f8695d39a2fecaa85854
Jinki Hou <jhou@smith.edu>
2024-10-29 22:09:36 -0400

    implementing house class

--- House.java
+++ House.java
@@ -7,14 +7,52 @@ public class House extends Building {
   private ArrayList<String> residents;
   private boolean hasDiningRoom;
 
-  public House(String name, String address, int nFloors) {
+  public House(String name, String address, int nFloors, boolean DiningRoom) {
     super(name, address, nFloors);
+    residents=new ArrayList<>();
+    residents.add(name);
+    hasDiningRoom=DiningRoom;
     System.out.println("You have built a house: 🏠");
+  }
+
+  public boolean hasDiningRoom(){
+    return hasDiningRoom;
+  }
+
+  public int nResidents(){
+    return residents.size();
+  }
+
+  public void moveIn(String name){
+    if(!residents.contains(name)){
+      residents.add(name);
+      System.out.println(name+" just moved in!");
+    }else{
+      System.out.println("Already lives here");
+    }
     
   }
 
+  public void moveOut(String name){
+    if(residents.contains(name)){
+      residents.remove(name);
+      System.out.println(name+" just moved out!");
+    }else{
+      System.out.println("couldn't find this resident");
+    }
+  }
+
+  public boolean isResident(String person){
+    return residents.contains(person);
+  }
+
   public static void main(String[] args) {
-    new House("morris", "Northampton", 3);
+    House Morris=new House("morris", "Northampton", 3, false);
+    System.out.println(Morris.hasDiningRoom());
+    Morris.moveIn("Kira");
+    Morris.moveOut("Kira");
+    Morris.moveOut("Kira");
+    System.out.println(Morris.isResident("Kira"));
   }
 
 }
